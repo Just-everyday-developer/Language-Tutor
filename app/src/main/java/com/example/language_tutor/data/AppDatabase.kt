26aa4d7cd522b72d8d174783dd8803affc2d5247
Daniel Li <149508426+dxxlee@users.noreply.github.com>
2025-02-06 22:22:03 +0600

    Update AppDatabase.kt

--- app/src/main/java/com/example/language_tutor/data/AppDatabase.kt
+++ app/src/main/java/com/example/language_tutor/data/AppDatabase.kt
@@ -1,1 +1,36 @@
+package com.example.test.data
+
+import android.content.Context
+import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+@Database(entities = [Word::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "word_database"
+                ).addCallback(object : Callback() {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
+                        super.onCreate(db)
+                        db.execSQL("INSERT INTO Word (word, translation, isLearned, level) VALUES ('apple', 'яблоко', 0, 'A1')")
+                        db.execSQL("INSERT INTO Word (word, translation, isLearned, level) VALUES ('table', 'стол', 0, 'A1')")
+                        db.execSQL("INSERT INTO Word (word, translation, isLearned, level) VALUES ('car', 'машина', 0, 'A2')")
+                        db.execSQL("INSERT INTO Word (word, translation, isLearned, level) VALUES ('universe', 'вселенная', 0, 'B2')")
+                    }
+                }).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
 
