c3bc60ddd49fa1511b3a249a187ab7aa3f5b7425
Daniel Li <149508426+dxxlee@users.noreply.github.com>
2025-02-06 22:22:29 +0600

    Create Word.kt

---
+++ app/src/main/java/com/example/language_tutor/data/Word.kt
@@ -0,0 +1,13 @@
+package com.example.test.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Word(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val word: String,
+    val translation: String,
+    val isLearned: Boolean = false,
+    val level: String
+)
